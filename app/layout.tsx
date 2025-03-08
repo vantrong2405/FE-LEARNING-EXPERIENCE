4a0ce3550e41f2a82afceb52fdcd23f65f584712
van trong <trongdn2405@gmail.com>
2025-03-08 11:42:36 +0700

    feat: add ui cart

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         </AppProvider>
         <AudioPlayer />
         <LazyLoading />
-        <NProgressHandler />
+        {/* <NProgressHandler /> */}
       </body>
     </html>
   )
